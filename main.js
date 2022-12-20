fe187372b501a874258059ade6b3679f4bea514d
Bart Bieszczad <bbart318@gmail.com>
2022-12-20 04:49:11 -0600

    Add matching password function

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('test')
+const pass = document.querySelector("#password")
+const confirmPass = document.querySelector("#confirm-password")
+const placeholder = document.getElementsByName('placeholder');
+
+function checkPassword() {
+    if (pass.value != "" && pass.value === confirmPass.value){
+        styleValid(pass)
+        styleValid(confirmPass)
+    } else if (pass.value != "" && pass.value != confirmPass.value) {
+        styleInvalid(pass)
+        styleInvalid(confirmPass)
+        }
+}
+
+function styleValid(input) {
+    input.style.backgroundColor = "#c6b8ef";
+    input.style.border = "1px solid #c6b8ef";
+}
+function styleInvalid(input) {
+    input.style.backgroundColor = "#f0c5c5";
+    input.style.border = "1px solid #f0c5c5";
+}
+
+confirmPass.addEventListener("input", checkPassword);
