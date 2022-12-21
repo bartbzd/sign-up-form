4a5f34dad04de4c3b8ef9cc919ade299f513a986
Bart Bieszczad <bbart318@gmail.com>
2022-12-21 03:04:25 -0600

    Add theme/icon functionality and refactor

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-console.log('test')
 const pass = document.querySelector("#password")
-const confirmPass = document.querySelector("#confirm-password")
-const placeholder = document.getElementsByName('placeholder');
+const checkPass = document.querySelector("#check-password")
+const passes = document.querySelectorAll('[type="password"]')
+const toggle = document.querySelector(".theme-button")
+const themeIcon = document.querySelector("#themeIcon")
+const colors = ["#c6b8ef", "#f0c5c5"];
 
 function checkPassword() {
-    if (pass.value != "" && pass.value === confirmPass.value){
-        styleValid(pass)
-        styleValid(confirmPass)
-    } else if (pass.value != "" && pass.value != confirmPass.value) {
-        styleInvalid(pass)
-        styleInvalid(confirmPass)
+    if (pass.value != "" && pass.value.length > 5 && pass.value === checkPass.value){
+        for (let i = 0; i < passes.length; i++) {
+        passes[i].style.backgroundColor = colors[0]
+        passes[i].style.border = `1px solid ${colors[0]}`
+        }
+    } else if (pass.value.length < 5 || pass.value != "" && pass.value != checkPass.value) {
+        for (let i = 0; i < passes.length; i++) {
+          passes[i].style.backgroundColor = colors[1]
+          passes[i].style.border = `1px solid ${colors[1]}`
+        }
         }
 }
 
-function styleValid(input) {
-    input.style.backgroundColor = "#c6b8ef";
-    input.style.border = "1px solid #c6b8ef";
-}
-function styleInvalid(input) {
-    input.style.backgroundColor = "#f0c5c5";
-    input.style.border = "1px solid #f0c5c5";
-}
-
-confirmPass.addEventListener("input", checkPassword);
+checkPass.addEventListener("input", checkPassword);
 pass.addEventListener("input", checkPassword);
+toggle.addEventListener("click", () => {
+    let theme = document.documentElement.getAttribute("data-theme");
+    let targetTheme = 'light';
+    if (theme === "light") {
+        targetTheme="dark"
+    }
+    document.documentElement.setAttribute("data-theme", targetTheme);
+
+    themeIcon.classList.toggle("fa-sun");
+    themeIcon.classList.toggle("fa-moon")
+})
